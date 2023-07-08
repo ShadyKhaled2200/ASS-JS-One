769c4343fcd19f3dbeceb933408854ca668307d9
shady <shadykhaled1235@gmail.com>
2023-07-08 19:32:00 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,378 @@
+
+// 1- Write a program that allow to user enter number then printit
+
+// var x = prompt('enter number ');
+// console.log(x);
+// document.getElementById('demo').innerHTML = x ;
+
+//000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+// 2- Write a program that take number from user then print yes if that number can divide by 3
+// and 4 otherwise print no
+
+// var x = prompt('enter number ');
+
+// if(x % 3 == 0){
+//     console.log("yes");
+// } else{
+//     console.log("noo");
+
+// }
+
+// 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+// 3- Write a program that allows the user to insert 2 integers then print the max
+
+// var x = Number(prompt('enter number x '));
+// var y = Number(prompt('enter number y '));
+
+// if(x > y){
+
+//     console.log(x);
+// }else{
+
+//     console.log("enter diff numbers");
+
+// }
+
+// 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+// 4- Write a program that allows the user to insert an integer then print negative if it is
+// negative number otherwise print positive.
+
+//  var x = Number(prompt('enter number x'));
+
+//  if(x < 0){
+
+//      console.log("negative");
+//  }else{
+
+//         console.log("positive");
+    
+//      }
+
+     // 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+
+//      5- Write a program that take 3 integers from user then print the max element
+// and the min element.
+
+    //  var x = Number(prompt('enter number x '));
+    // var y = Number(prompt('enter number y '));
+    // var z = Number(prompt('enter number z '));
+
+    //  if(x > y && x > z && y < x && y < z){
+
+    // console.log("max element is " + x);
+    // console.log("min element is " + y);
+
+    // } else if (y > x && y > z && x < y && x < z){
+
+    //     console.log("max element is " + y);
+    //     console.log("min element is " + x);
+    
+    //     } else if (z > x && z > y && y < x && y < z){
+
+    //         console.log("max element is " + z);
+    //         console.log("min element is " + y);
+        
+    //     }else if (y > x && y > z && z < x && z < y){
+
+    //             console.log("max element is " + y);
+    //             console.log("min element is " + z);
+
+
+    //     } else if (x > z && x > y && z < x && z < y){
+
+    //         console.log("max element is " + x);
+    //         console.log("min element is " + z);
+        
+            
+    //     } else if (z > x && z > y && x < y && x < z){
+
+    //         console.log("max element is " + z);
+    //         console.log("min element is " + x);
+         
+    //         } else{
+    //             console.log("try again...");
+
+    //         }
+
+            // 000000000000000000000000000000000000000000000000000000
+
+//             6-7- Write a program that allows the user to insert integer number then
+// check If a number is oven or odd
+
+    //  var x = Number(prompt('enter number '));
+
+//  if(x % 2 == 0){
+//     console.log("even");
+//  } else if(x % 2 != 0){
+//      console.log("odd");
+
+//  }
+
+
+            // 000000000000000000000000000000000000000000000000000000000000000000
+
+            // 8- Write a program that take character from user then if it is vowel chars (a,e,I,o,u)
+            // then print vowel otherwise print consonant
+            
+
+    //  var x = prompt('enter letter');
+
+    //    if(x == "o" || x == "e" || x == "a" || x == "u" || x == "I" || x == "i"){
+    // console.log("vowel");
+
+    //    }else{
+
+    //    }
+       
+                   // 000000000000000000000000000000000000000000000000000000000000000000
+
+                //    9- Writeaprogramthat allowsusertoinsertintegerthenprintallnumbersbetween1to
+                //    that’s number
+
+
+//                          var x = Number(prompt('enter number'));
+
+// for(i = 1 ; i <= x ; i++ ){
+
+//          console.log(i)
+
+// }
+  
+                   // 000000000000000000000000000000000000000000000000000000000000000000
+
+                //    10- Write a program that allows userto insert integerthen print a multiplication table up to 12.
+
+                    //   var num = Number((prompt("Enter an integer:")));
+
+
+                    //   for(i = 1 ; i <= 12; i++){
+                    //     var result = num * i;
+                    //     console.log(result);
+
+                    //   }
+
+                                         // 000000000000000000000000000000000000000000000000000000000000000000
+
+                                        //  11- Write aprogram that allows to user to insert number then print all even numbers
+                                        //  between 1 to this number 
+
+
+//                 var x = Number(prompt('enter number'));
+
+//  for(i = 2 ; i <= x ; i+=2 ){
+
+//           console.log(i)
+
+//  }
+
+                    // 000000000000000000000000000000000000000000000000000000000000000000
+
+// 11- Write a program that take two integers then print thepower
+
+// var base = Number(prompt("enter el base..."))
+// var pow = Number(prompt("enter el pow..."))
+// var res = 1 ;
+
+// for(i = 1; i <= pow; i++){
+//     res = base *res ;
+// }
+
+// console.log("result is " + res);
+
+                    // 000000000000000000000000000000000000000000000000000000000000000000
+
+
+// 12- Write a program to enter marks of five subjects and calculate total, average and
+// percentage.
+
+
+//  var num1 = Number(prompt("enter num..."));
+//  var num2 = Number(prompt("enter num..."));
+//  var num3 = Number(prompt("enter num..."));
+//  var num4 = Number(prompt("enter num..."));
+//  var num5 = Number(prompt("enter num..."));
+
+// var total = num1 + num2 + num3 + num4 + num5;
+// var avr = total / 5 ;
+// var percentage = (total / 500) * 100;
+
+// console.log("total is " + total);
+// console.log("avr is " + avr);
+// console.log("percentage is " + percentage);
+
+        // 000000000000000000000000000000000000000000000000000000000000000000
+
+// 13-Write a program to input month number and print number of days in that
+// month.
+
+
+
+
+
+
+
+
+
+        // 000000000000000000000000000000000000000000000000000000000000000000
+
+
+// 14- Write a program to input marks of five subjects
+// Physics, Chemistry, Biology, Mathematics and Computer
+// , Find percentage and grade 
+
+
+//       var subj = prompt('enter subj');
+// if(subj >= 90){
+//     console.log("Grad A");
+// }else if(subj >= 80){
+//     console.log("Grad B");
+// }
+// else if(subj >= 70){
+//     console.log("Grad C");
+// }
+// else if(subj >= 60){
+//     console.log("Grad D");
+// }
+// else if(subj >= 40){
+//     console.log("Grad E");
+// }
+// else if(subj < 40){
+//     console.log("Grad F");
+// }
+
+        // 000000000000000000000000000000000000000000000000000000000000000000
+
+
+
+
+                    // *******************************TODO Using switch case*******************************
+
+// 15- Write a program to print total number of days in month
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // 000000000000000000000000000000000000000000000000000000000000000000
+
+
+
+
+                    // 16- Write a program to check whether an alphabet is vowel or consonant 
+
+                    //   var x = (prompt("Enter an integer:"));
+
+                    // switch(x){
+                    //     case "a":
+                    //     case "e":
+                    //     case "o":
+                    //     case "O":
+                    //     case "u":
+                    //     case "i":
+                    //     case "I":
+                    //         console.log("vowel");
+                    //         break;
+
+                    //     default:  console.log("const");
+
+                    // }
+
+
+                    // 000000000000000000000000000000000000000000000000000000000000000000
+
+                    // 17- Write a program to find maximum between two numbers 
+
+                //  var x = Number(prompt('enter number x'));
+                //  var y = Number(prompt('enter number y'));
+                //  switch(true){
+                //     case x > y :
+                //      console.log("x is max = " + x);
+                //     break;
+                //     case x < y :
+                //      console.log("y is max = " + y);
+                //     break;
+                //     default:  console.log("try again .....");
+
+                //  }
+
+                
+                // 000000000000000000000000000000000000000000000000000000000000000000
+
+
+                // 18- Write a program to check whether a number is even or odd 
+
+                // var x = Number(prompt('enter number x'));
+
+
+                // switch(true){
+                //     case x % 2 == 0 :
+                //      console.log("x is even = " + x);
+                //     break;
+                //     case x % 2 != 0 :
+                //      console.log("x is odd = " + x);
+                //     break;
+                //     default:  console.log("try again .....");
+                // }
+
+                // 000000000000000000000000000000000000000000000000000000000000000000
+
+                // 19- Write a program to check whether a number is positive or negative or zero
+
+                // var x = Number(prompt('enter number x'));
+
+
+                // switch(true){
+                //     case x == 0 :
+                //      console.log("x is zero = " + x);
+                //     break;
+                //     case x > 0 :
+                //      console.log("x is positive = " + x);
+                //     break;
+                //     case x < 0 :
+                //      console.log("x is negative = " + x);
+                //     break;
+                //     default:  console.log("try again .....");
+                // }
+
+            // 000000000000000000000000000000000000000000000000000000000000000000
+
+            // 20- Write a program to create Simple Calculator
+
+                //  var x = Number(prompt('enter number x'));
+                //  var y = Number(prompt('enter number y'));
+                //  var oper = prompt('enter number oper');
+                //  var res ;
+
+                //  switch(oper){
+                //     case "+" : 
+                //     res = x + y;
+                //     break;
+                //     case "*" : 
+                //     res = x * y;
+                //     break;
+                //     case "/":
+                //     res = num1 / num2;
+                //         break;
+
+                //  default:  console.log("try again .....");
+                        
+                //  } 
+
+                //  console.log("Result : " + res);
+
+                             // 000000000000000000000000000000000000000000000000000000000000000000
+
+
